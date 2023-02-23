22e9a2d8f48ce8e3a98c1f6d322d000273023272
laggraw <agg.lalit@gmail.com>
2023-02-23 21:33:56 +0530

    visibility logic for logout link modified

--- src/test/java/pages/actiTIME_Enter_Time_TrackPage.java
+++ src/test/java/pages/actiTIME_Enter_Time_TrackPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class actiTIME_Enter_Time_TrackPage {
 	WebDriver driver;
@@ -16,8 +18,15 @@ public class actiTIME_Enter_Time_TrackPage {
 		PageFactory.initElements(driver, this);
 	}
 	
-	public boolean validateLogoutLink() {
-		return logout.isDisplayed();
+	public boolean validateLogoutLink(WebDriverWait wait) {
+		try {
+		wait.until(ExpectedConditions.visibilityOf(logout));
+		return true;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 }
