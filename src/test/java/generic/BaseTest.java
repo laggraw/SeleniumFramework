3bff39680d6cbf212bb7eb17368d3f0b1512fd7b
laggraw <agg.lalit@gmail.com>
2023-02-24 14:34:35 +0530

    Updated for remote browsers

--- src/test/java/generic/BaseTest.java
+++ src/test/java/generic/BaseTest.java
@@ -1,10 +1,15 @@
 package generic;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
@@ -18,17 +23,23 @@ public class BaseTest {
 	public final String dBrowser = "chrome";
 	public final String dUrl = "https://demo.actitime.com/login.do";
 	public final String DTimeout = "10";
+	public final String dGridUrl = "http://192.168.29.205:4444";
 	
-	
-	@Parameters({"browser", "url", "timeout"})
+	@Parameters({"browser", "gridurl", "url", "timeout"})
 	@BeforeMethod
-	public void opeApp(@Optional(dBrowser)String browser, @Optional(dUrl)String url, @Optional(DTimeout)String timeout) {
+	public void opeApp(@Optional(dBrowser)String browser, @Optional(dGridUrl)String gridurl, @Optional(dUrl)String url, @Optional(DTimeout)String timeout) throws MalformedURLException {
+		
+		URL remote = new URL(gridurl);
 		Reporter.log("Opening the Browser : " + browser , true);
 		if(browser.equalsIgnoreCase("chrome")) {
-			driver = new ChromeDriver();	
+			
+//			driver = new ChromeDriver();	
+			driver = new RemoteWebDriver(remote, new ChromeOptions());
+			
 		}
 		else if(browser.equalsIgnoreCase("firefox")) {
-			driver = new FirefoxDriver();
+//			driver = new FirefoxDriver();
+			driver = new RemoteWebDriver(remote, new FirefoxOptions());
 		}
 		Reporter.log("Maximizing the browser", true);
 		driver.manage().window().maximize();
